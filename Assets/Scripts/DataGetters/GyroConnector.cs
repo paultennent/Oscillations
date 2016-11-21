e5882cf9a7e938d067a6281d61952220cc1eb418
Joe Marshall <joe.marshall@nottingham.ac.uk>
2016-11-21 16:08:43 +0000

    update gyro connector with buffering stuff

--- Assets/Scripts/DataGetters/GyroConnector.cs
+++ Assets/Scripts/DataGetters/GyroConnector.cs
@@ -5,9 +5,23 @@ using System.Net.Sockets;
 using System;
 using UnityEngine;
 
+
+
 public class GyroConnector  
 {
-    public UdpClient receiver;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    const int PACKET_SIZE=28;
+#else
+    const int PACKET_SIZE=24;
+    float timeLastPoll=0;
+    IPEndPoint serverEndPoint=new IPEndPoint(IPAddress.Parse("10.154.161.18"),2323);
+
+
+#endif
+
+    public Socket receiver;
+    byte[] receiveBytes=new byte[PACKET_SIZE];
     
     public float mAngle=0;
     public float mAngularVelocity=0;
@@ -18,10 +32,11 @@ public class GyroConnector
 
 	public void init () 
     {
-        // make sure gyro service is running
-#if UNITY_ANDROID && !UNITY_EDITOR
-        receiver=new UdpClient(2424);
 
+        receiver=new Socket(AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+        receiver.Bind(new IPEndPoint(IPAddress.Any,2424));
+
+#if UNITY_ANDROID && !UNITY_EDITOR
         AndroidJavaClass activityClass;
         AndroidJavaObject activity, intent;
                    
@@ -33,13 +48,11 @@ public class GyroConnector
         intent.Call<AndroidJavaObject>("setClassName","com.mrl.simplegyroclient","com.mrl.simplegyroclient.GyroClientService");
         Debug.Log("woo:3"+intent);
         activity.Call<AndroidJavaObject>("startService",intent);
-
-
-
-		receiver.BeginReceive(new AsyncCallback(ReceiveCallback),receiver);
-
-
+#else
+        timeLastPoll=Time.time;
 #endif
+
+
 	}
     
     public void stop()
@@ -56,14 +69,14 @@ public class GyroConnector
         intent.Call<AndroidJavaObject>("setClassName","com.mrl.simplegyroclient","com.mrl.simplegyroclient.GyroClientService");
         Debug.Log("woo:3"+intent);
         activity.Call<AndroidJavaObject>("stopService",intent);
-        
 #endif
     }
 	
-    // java is big endian, sorry
-    static byte[] conversionBuf4={0,0,0,0};
-    static byte[] conversionBuf8={0,0,0,0,0,0,0,0};
-    static float getBigEndianFloat(byte[]input,int offset)
+    // java is big endian, these are some conversion helpers
+    
+    byte[] conversionBuf4={0,0,0,0};
+    byte[] conversionBuf8={0,0,0,0,0,0,0,0};
+    float getBigEndianFloat(byte[]input,int offset)
     {
         if(BitConverter.IsLittleEndian)
         {
@@ -75,7 +88,7 @@ public class GyroConnector
             return BitConverter.ToSingle(input,offset);
         }
     }
-	static int getBigEndianInt32(byte[]input,int offset)
+	int getBigEndianInt32(byte[]input,int offset)
     {
         if(BitConverter.IsLittleEndian)
         {
@@ -88,7 +101,7 @@ public class GyroConnector
         }
         
     }
-	static long getBigEndianInt64(byte[]input,int offset)
+	long getBigEndianInt64(byte[]input,int offset)
     {
         if(BitConverter.IsLittleEndian)
         {
@@ -101,54 +114,129 @@ public class GyroConnector
         }
     }
     
-	static IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    EndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
+    float[] mSpareAngles=new float[10];
+    int mSpareAngleCount=0;
+    int mSpareAngleReadPos=0;
 
-	public static void ReceiveCallback(IAsyncResult ar)
-	{
-		GyroConnector pThis = (GyroConnector)ar.AsyncState;
-		UdpClient u = ((GyroConnector)(ar.AsyncState)).receiver;
-
-		Byte[] receiveBytes = u.EndReceive(ar, ref RemoteIpEndPoint);
-
-		if(receiveBytes.Length>=28)
-		{
-			pThis.mAngle=getBigEndianFloat(receiveBytes,0);
-			pThis.mAngularVelocity=getBigEndianFloat(receiveBytes,4);
-			pThis.mMagDirection=getBigEndianFloat(receiveBytes,8);
-			pThis.mRemoteBatteryLevel=getBigEndianFloat(receiveBytes,12);
-			pThis.mTimestamp=getBigEndianInt64(receiveBytes,16);               
-			pThis.mLocalBatteryLevel=getBigEndianFloat(receiveBytes,24);
-		}            
-
-
-		u.BeginReceive (new AsyncCallback (ReceiveCallback), ar.AsyncState);
-	}
-
+    
+    class AngleTime
+    {
+        public float angle;
+        public float time;
+    };
+    
+    AngleTime []mBuffer=new AngleTime[10];
+    int mBufferCount=0;
+    
+    bool firstTime=true;
+    long firstTimestamp=0;
+    float firstUnityTime=0;
+    
+    float unityDelay=0.0f;
+    
 	public void readData() 
     {
-		receiver.BeginReceive (new AsyncCallback(ReceiveCallback),this);
-/*		const int FRAME_DELAY = 2;
-        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-		while(receiver.Available>=28)
-//		for(int c=0;c<5 && receiver!=null && receiver.Available>=28;c++)
+#if UNITY_ANDROID && !UNITY_EDITOR
+#else
+    //   if we're running in editor, need to poll server to get messages
+        if(Time.time-timeLastPoll>0.5)
         {
-            Byte[] receiveBytes = receiver.Receive(ref RemoteIpEndPoint);
-            if(receiveBytes.Length>=28)
+            byte[] launchPacket={1,2,3,4};
+            receiver.SendTo(launchPacket,serverEndPoint);
+//            Debug.Log("Polling gyro");
+            timeLastPoll=Time.time;
+        }
+#endif
+
+
+        int count=0;
+        // receive everything 
+        // send it out as fast as possible with zero missing frames
+        
+    	while(receiver.Available>=PACKET_SIZE)
+        {
+            int len=receiver.ReceiveFrom(receiveBytes,ref remoteIpEndPoint);
+            if(receiveBytes.Length>=PACKET_SIZE)
             {
-                mAngle=getBigEndianFloat(receiveBytes,0);
+                float angle=getBigEndianFloat(receiveBytes,0);
+                long timestamp=getBigEndianInt64(receiveBytes,16);
+                if(firstTime)
+                {
+                    firstTime=false;
+                    firstTimestamp=getBigEndianInt64(receiveBytes,16);
+                    firstUnityTime=Time.time;
+                    mAngle=angle;
+                    mTimestamp=timestamp;
+                    for(int c=0;c<mBuffer.Length;c++)
+                    {
+                        mBuffer[c]=new AngleTime();
+                    }
+                }
+                float timestampInUnity=firstUnityTime+0.000000001f*(float)(timestamp-firstTimestamp);
+    //            Debug.Log(timestampInUnity+"!!!");
+                if(mBufferCount<mBuffer.Length)
+                {
+                    mBuffer[mBufferCount].angle=angle;
+                    mBuffer[mBufferCount].time=timestampInUnity;
+                    mBufferCount++;
+                }
                 mAngularVelocity=getBigEndianFloat(receiveBytes,4);
                 mMagDirection=getBigEndianFloat(receiveBytes,8);
                 mRemoteBatteryLevel=getBigEndianFloat(receiveBytes,12);
-                mTimestamp=getBigEndianInt64(receiveBytes,16);               
+        #if UNITY_ANDROID && !UNITY_EDITOR
                 mLocalBatteryLevel=getBigEndianFloat(receiveBytes,24);
+        #endif
             }            
-        }*/
-        // get angle and other things
-        // print it here
-        // TODO: check mag direction code
-        // TODO: get battery level
-        // TODO: put this into Paul's code
-        // TODO: bluetooth fallback
+        }
+        
+        if(mBufferCount>0)
+        {
+            float unityTimeNow=Time.time-firstUnityTime - unityDelay;
+  //          Debug.Log(unityTimeNow+":"+mBuffer[0].time+"!"+mBufferCount+"["+mBuffer[0].angle);
+            int bufPos=0;
+            while(bufPos<mBuffer.Length && bufPos<mBufferCount && mBuffer[bufPos].time<unityTimeNow)
+            {
+                mAngle=mBuffer[bufPos].angle;
+                bufPos++;
+            }
+            // shift used points in buffer off
+            int d=0;
+            for(int c=bufPos;c<mBufferCount;c++)
+            {
+                mBuffer[d].angle=mBuffer[c].angle;
+                mBuffer[d].time=mBuffer[c].time;
+                d++;
+            }
+            mBufferCount-=bufPos;
+            if(mBufferCount>3)
+            {
+                // big buffer, drop latency
+                unityDelay-=0.01f;
+//              Debug.Log(unityTimeNow+":"+mBuffer[0].time+"!"+mBufferCount+"["+mBuffer[0].angle);
+
+            }
+            if(mBufferCount<2)
+            {
+                // dropping frames, up latency slightly
+                unityDelay+=0.001f;
+            }
+            if(mBufferCount==10)
+            {
+                // set delay to middle of buffer
+                unityDelay=Time.time-firstUnityTime - mBuffer[4].time;
+            }
+        }else
+        {
+            Debug.Log("Dropped frame");
+        }
+//        Debug.Log("Buf size:"+mBufferCount);
 	}
+
+
+
+
 }
+
+
