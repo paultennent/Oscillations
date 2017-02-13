75e813bacc0d0635f7ae7745304ab383b0513018
paultennent <paultennent@nottingham.ac.uk>
2017-02-13 22:57:28 +0000

    Added AI Jellys

---
+++ Assets/Rides/JellyfishAssets/JellySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellySpawner : MonoBehaviour {
+
+	public GameObject jellyPrefab;
+	public int count;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < count; i++) {
+			Instantiate (jellyPrefab);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
