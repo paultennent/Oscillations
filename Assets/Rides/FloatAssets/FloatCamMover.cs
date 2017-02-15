c5e96e46863c877461d09f37a78273062a4d861c
paultennent <paultennent@nottingham.ac.uk>
2017-02-15 10:26:49 +0000

    float first cut, jelly tweaks

---
+++ Assets/Rides/FloatAssets/FloatCamMover.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatCamMover : AbstractGameEffects {
+
+	private float curHeight=0f;
+	private float yVelocity=0f;
+
+	public float velocityDragConstant=.00003f;
+	public  float upforceConstant=.001f;
+	public  float gravityConstant=9.8f;
+
+	private Vector3 initialPosition;
+
+	public GameObject pivot;
+
+	public bool launch=false;
+
+	public bool infiniteFall = true;
+
+	private Skirt skirtObj;
+
+	// Use this for initialization
+	void Start () {
+		base.Start();
+		initialPosition=pivot.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		base.Update();
+		float upforce=calculateUpforce();
+
+		yVelocity+=upforce*Time.deltaTime;
+		curHeight=curHeight+yVelocity*Time.deltaTime;
+		if(curHeight<0 & !infiniteFall)
+		{
+			curHeight=0;
+			yVelocity=0;
+		}
+		pivot.transform.position=initialPosition+new Vector3(0,curHeight,0);
+	
+	}
+
+	float calculateUpforce()
+	{
+		float totalForce=swingAngVel*swingAngVel*upforceConstant;
+		if(launch==true)
+		{
+			totalForce=20f;
+		}
+
+		totalForce-=gravityConstant;
+		if(yVelocity>0)
+		{
+			totalForce-=(yVelocity*yVelocity)*velocityDragConstant;
+		}else
+		{
+			totalForce+=(yVelocity*yVelocity)*velocityDragConstant;
+		}
+		return totalForce;
+	}
+}
--- Assets/Scripts/Misc/FishAI.cs
+++ Assets/Scripts/Misc/FishAI.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FishAI : MonoBehaviour {
 
 	private Transform fishParent;
-	private float spawnRange = 200f;
+	public float spawnRange = 200f;
 	private float speed = 2f;
 
 	private float rotSpeed = 2f;
@@ -14,13 +14,16 @@ public class FishAI : MonoBehaviour {
 	public Animation anim;
 	private Transform root;
 
+	private float ceiling = 1000f;
+	private float floor = -1000f;
+
 	// Use this for initialization
 	void Start () {
 		fishParent = GameObject.Find ("Fish").transform;
 		root = GameObject.Find ("Centre").transform;
 		transform.parent = fishParent;
-		transform.localPosition = new Vector3 (Random.Range (root.position.x-spawnRange, root.position.x+spawnRange), Random.Range (root.position.y-spawnRange, root.position.y+spawnRange), Random.Range (root.position.z-spawnRange, root.position.z+spawnRange));
-		target = new Vector3 (Random.Range (root.position.x-spawnRange, root.position.x+spawnRange), Random.Range (root.position.y-spawnRange, root.position.y+spawnRange), Random.Range (root.position.z-spawnRange, root.position.z+spawnRange));
+		transform.localPosition = new Vector3 (Random.Range (root.position.x-spawnRange, root.position.x+spawnRange), Random.Range (Mathf.Max(floor,root.position.y-spawnRange), Mathf.Min(ceiling,root.position.y+spawnRange)), Random.Range (root.position.z-spawnRange, root.position.z+spawnRange));
+		target = new Vector3 (Random.Range (root.position.x-spawnRange, root.position.x+spawnRange), Random.Range (Mathf.Max(floor,root.position.y-spawnRange), Mathf.Min(ceiling,root.position.y+spawnRange)), Random.Range (root.position.z-spawnRange, root.position.z+spawnRange));
 
 		speed = Random.Range (2f, 7.5f);
 		rotSpeed = Random.Range (2f, 10f);
@@ -32,7 +35,7 @@ public class FishAI : MonoBehaviour {
 	void Update () {
 		//new target if we ever get close
 		if (Vector3.Distance (transform.position, target) < 2) {
-			target = new Vector3 (Random.Range (root.position.x-spawnRange, root.position.x+spawnRange), Random.Range (root.position.y-spawnRange, root.position.y+spawnRange), Random.Range (root.position.z-spawnRange, root.position.z+spawnRange));
+			target = new Vector3 (Random.Range (root.position.x-spawnRange, root.position.x+spawnRange), Random.Range (Mathf.Max(floor,root.position.y-spawnRange), Mathf.Min(ceiling,root.position.y+spawnRange)), Random.Range (root.position.z-spawnRange, root.position.z+spawnRange));
 		}
 
 		SmoothLookAt(target);
@@ -44,5 +47,13 @@ public class FishAI : MonoBehaviour {
 		Vector3 dir = t - transform.position;
 		Quaternion targetRotation = Quaternion.LookRotation(dir);
 		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * rotSpeed);
+	}
+
+	public void setCeiling(float i){
+		ceiling = i;
+	}
+
+	public void setFloor(float i){
+		floor = i;
 	}
 }
