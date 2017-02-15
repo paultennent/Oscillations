523d58fae11409caa617717fec19296d8aac1d6e
Joe Marshall <joe.marshall@nottingham.ac.uk>
2017-02-15 10:25:18 +0000

    double sided tracks and things

--- Assets/Rides/JellyfishAssets/Skirt.cs
+++ Assets/Rides/JellyfishAssets/Skirt.cs
@@ -11,11 +11,16 @@ public class Skirt : MonoBehaviour {
 	public float height = 2f;
 
 	void Start () {
-		GetComponent<MeshFilter>().sharedMesh = Create(subdivisions);
+        currentSubdivisions=0;
+//		GetComponent<MeshFilter>().sharedMesh = Create(subdivisions);
 	}
 	
 	void Update () {
-         UpdateMeshVertices(GetComponent<MeshFilter>().sharedMesh,currentSubdivisions);
+        if(currentSubdivisions!=subdivisions)
+        {
+            GetComponent<MeshFilter>().sharedMesh = Create(subdivisions);
+        }
+        UpdateMeshVertices(GetComponent<MeshFilter>().sharedMesh,currentSubdivisions);
 	}
     
     Vector3 []vertices;
@@ -37,6 +42,9 @@ public class Skirt : MonoBehaviour {
             vertices[i] = new Vector3(x1,height, z1);
             // bottom circle
             vertices[i+subdivisions] = new Vector3(x2, 0f, z2);
+            
+            // bottom inside circle
+            vertices[i+subdivisions*2]=new Vector3(x2*0.99f,0f,z2*0.99f);
         }
         mesh.vertices=vertices;
 		mesh.RecalculateBounds();
@@ -48,16 +56,17 @@ public class Skirt : MonoBehaviour {
         Mesh mesh = new Mesh();
         currentMesh=mesh;
 
-		vertices = new Vector3[subdivisions*2 + 2];
+		vertices = new Vector3[subdivisions*3 + 2]; 
 		Vector2[] uv = new Vector2[vertices.Length];
-		int[] triangles = new int[(subdivisions * 2) * 3];
+		int[] triangles = new int[(subdivisions * 4) * 3];
 
 		for(int i = 0, n = subdivisions - 1; i < subdivisions; i++) {
 			float ratio = (float)i / n;
 			uv[i ] = new Vector2(ratio, 0f);
 			uv[i+subdivisions ] = new Vector2(ratio, 1f);
+			uv[i+(subdivisions*2) ] = new Vector2(ratio, 1f);
 		}
-		// construct sides
+		// construct outsides
 		for(int i = 0, n = subdivisions - 1; i < n; i++) {
 			int offset = i * 6;
 			triangles[offset + 0] = i ; 
@@ -68,6 +77,19 @@ public class Skirt : MonoBehaviour {
 			triangles[offset + 4] = i+subdivisions ; 
 			triangles[offset + 5] = i + 1+subdivisions; 
 		}
+
+		// construct insides
+		for(int i = 0, n = subdivisions - 1; i < n; i++) {
+			int offset = (i+subdivisions) * 6;
+			triangles[offset + 2] = i ; 
+			triangles[offset + 1] = i+subdivisions*2 ; 
+			triangles[offset + 0] = i + 1; 
+            
+			triangles[offset+5 ] = i+1 ; 
+			triangles[offset + 4] = i+subdivisions*2 ; 
+			triangles[offset + 3] = i + 1+subdivisions*2; 
+		}
+
         
         UpdateMeshVertices(mesh,subdivisions);
 		mesh.uv = uv;
--- Assets/Rides/Pachinko/TrackGenerator.cs
+++ Assets/Rides/Pachinko/TrackGenerator.cs
@@ -9,20 +9,27 @@ public class TrackGenerator : MonoBehaviour {
     public float radiusLoop = 20f;
     public float gapPercent = 5f;
     public float width=2f;
+    public float thickness=1f;
 
     public bool generateNew=false;
+    
+    private float []extrusionX={1,-1,-1,1};
+    private float []extrusionY={1,1,-1,-1};
+   
+   
+    const int NUM_POINTS=41;
     
 	void Start () {
 		GetComponent<MeshFilter>().sharedMesh = Create();
 	}
 	
 	void Update () {
-         UpdateMeshVertices(GetComponent<MeshFilter>().sharedMesh);
+         UpdateMeshVertices(GetComponent<MeshFilter>().sharedMesh,null);
          if(generateNew)
          {
              generateNew=false;
-             CreateNewSegment(22,0);
-//             CreateNewSegment(45,40);
+//             CreateNewSegment(22,0);
+             CreateNewSegment(45,40);
          }
 	}
     
@@ -43,23 +50,44 @@ public class TrackGenerator : MonoBehaviour {
     int currentSubdivisions;
     Mesh currentMesh;
     
-    void UpdateMeshVertices(Mesh mesh)
+    void UpdateMeshVertices(Mesh mesh,Vector2[] uv)
     {        
-        vertices[0]=new Vector3(-width,0,0);
-        vertices[1]=new Vector3(width,0,0);
+        int pointsPerSlice=extrusionX.Length;
+        //start ramp
+        for(int k=0;k<pointsPerSlice;k++)
+        {
+            float texX=k/(float)pointsPerSlice;
+            vertices[k]=new Vector3(extrusionX[k]*width,-extrusionY[k]*thickness,0);
+            if(uv!=null)
+            {
+                uv[k]=new Vector2(texX,0);
+            }
+        }      
         float centreY=radiusLoop;
         float centreZ=lengthInlet;
-        float n = vertices.Length-2;
-        float mult=1f-0.01f*gapPercent;
-        for(int i=2;i<vertices.Length;i+=2)
+        //loop
+        float mult = 1.0f-gapPercent*0.01f;
+        float n = vertices.Length-pointsPerSlice;        
+        for(int i=pointsPerSlice;i<vertices.Length;i+=pointsPerSlice)
         {
-            float ratio = (float)(i-2) / n;
+            float ratio = (float)(i-4) / n;
             float r = mult*ratio * (Mathf.PI * 2f);
-            float y=centreY-Mathf.Cos(r)*radiusLoop;
-            float z=Mathf.Sin(r)*radiusLoop + centreZ;
-            vertices[i]=new Vector3(-width,y,z);
-            vertices[i+1]=new Vector3(width,y,z);
+            float cosR=Mathf.Cos(r);
+            float sinR=Mathf.Sin(r);
+            
+            for(int k=0;k<pointsPerSlice;k++)
+            {
+                float texX=k/(float)pointsPerSlice;
+                float y=centreY-cosR*(radiusLoop+extrusionY[k]*thickness);
+                float z=sinR*(radiusLoop+extrusionY[k]*thickness) + centreZ;
+                vertices[i+k]=new Vector3(extrusionX[k]*width,y,z);
+                if(uv!=null)
+                {
+                    uv[i+k]=new Vector2(texX,ratio);
+                }
+            }
         }
+
         mesh.vertices=vertices;
 		mesh.RecalculateBounds();
 		mesh.RecalculateNormals();
@@ -69,37 +97,31 @@ public class TrackGenerator : MonoBehaviour {
         
         Mesh mesh = new Mesh();
         currentMesh=mesh;
+        int pointsPerSlice=extrusionX.Length;
 
-        // vertices:
-        // 40 on either side of the loop        
-        // 2 into it
-        // gap in the loop just before the entrance
-                
-        
-		vertices = new Vector3[41*2];
+		vertices = new Vector3[NUM_POINTS*pointsPerSlice];
 		Vector2[] uv = new Vector2[vertices.Length];
-		int[] triangles = new int[(vertices.Length-1)*3];
-        float n=vertices.Length;
-        for(int i=0;i<vertices.Length-2;i+=2)
+		int[] triangles = new int[(NUM_POINTS-1)*6*pointsPerSlice];
+        // build triangle extrusion
+        for(int i=0;i<NUM_POINTS-1;i++)
         {
-            float ratio = (float)i / n;
-
-            int offset=i*3;
-			triangles[offset + 0] = i ; 
-			triangles[offset + 1] = i+1 ; 
-			triangles[offset + 2] = i + 2; 
-
-			triangles[offset+3 ] = i+2 ; 
-			triangles[offset + 4] = i+1 ; 
-			triangles[offset + 5] = i + 3; 
-            
-            uv[i]=new Vector2(0,ratio);
-            uv[i+1]=new Vector2(1,ratio);
+            for(int k=0;k<pointsPerSlice;k++)
+            {
+                int offset= (i*pointsPerSlice + k)*6;
+                int k2 = (k+1)%pointsPerSlice;
+                int p1=i*pointsPerSlice+k;
+                int p2=i*pointsPerSlice+k2;
+                int p3=(i+1)*pointsPerSlice+k;
+                int p4=(i+1)*pointsPerSlice+k2;
+                triangles[offset + 0] = p1 ; 
+                triangles[offset + 1] = p3 ; 
+                triangles[offset + 2] = p2; 
+                triangles[offset+3 ] = p2 ; 
+                triangles[offset + 4] = p3 ; 
+                triangles[offset + 5] = p4; 
+            }
         }
-        uv[vertices.Length-2]=new Vector2(0,1);
-        uv[vertices.Length-1]=new Vector2(1,1);
-        
-        UpdateMeshVertices(mesh);
+        UpdateMeshVertices(mesh,uv);
 		mesh.uv = uv;
 		mesh.triangles = triangles;
 		mesh.RecalculateBounds();
