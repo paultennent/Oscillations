f27a2df00784d0a65851ee31f58e8b190863d9a7
Joe Marshall <joe.marshall@nottingham.ac.uk>
2017-02-17 12:57:22 +0000

    changes to cam mover for walker.unity

--- Assets/Rides/WalkerAssets/WalkerCamMover.cs
+++ Assets/Rides/WalkerAssets/WalkerCamMover.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// walk in 4 phases:
+// 1 Left leg bend up (does right leg bend so that body stays straight here?) // swing angle 0 -> 1
+// 2 Right leg bends until left leg touches down                              // swing angle 1 -> 0
+// 3 Right leg bends up                                                       // swing angle 0 -> -1
+// 4 Left leg bends until right leg touches down                              // swing angle -1 -> 0
+
+// on terrain, raycast down from each leg
+// if it hits terrain, then lift it up
+// lift up to minimum that gets lowest leg out
+// then rotate until highest leg out
+
 public class WalkerCamMover : AbstractGameEffects  
 {
     
     public Transform leftFoot;
     public Transform rightFoot;
+    public Transform leftHip;
+    public Transform rightHip;
     
     public Transform body;
     
@@ -15,6 +28,9 @@ public class WalkerCamMover : AbstractGameEffects
     
     public float rockMultiplier=0.25f;
     public float twistMultiplier=0.25f;
+    
+    public float leftAngle=0f;
+    public float rightAngle=0f;
     
 	// Use this for initialization
 	void Start () 
@@ -26,7 +42,7 @@ public class WalkerCamMover : AbstractGameEffects
     bool seenZeroCrossing=false;
     bool firstStep=true;
     
-    float lastAngle=0;
+    float prevAngle=0;
     float maxAngle=0;
 	// Update is called once per frame
 	void Update () 
@@ -34,7 +50,7 @@ public class WalkerCamMover : AbstractGameEffects
         
         base.Update();
         // are we going left-> right or right->left
-        if((lastAngle<0 && swingAngle>=0) || (lastAngle>=0 && swingAngle<0))
+        if((prevAngle<0 && swingAngle>=0) || (prevAngle>=0 && swingAngle<0))
         {
             if(!seenZeroCrossing)
             {
@@ -44,20 +60,37 @@ public class WalkerCamMover : AbstractGameEffects
             {
                 firstStep=false;
             }
-        }
+        }                
         if(seenZeroCrossing)
         {
-            float twistAmount = Mathf.Abs(swingAngVel*twistMultiplier*Time.deltaTime);
+            float legAngle=swingAngle;
+            Transform lockFoot=rightFoot;
+            if(swingQuadrant==1 || swingQuadrant==2)
+            {
+                lockFoot=leftFoot;
+            }
+            Vector3 footPos=lockFoot.position;
+            // set hip angles
+            
+            
+            leftHip.localEulerAngles=new Vector3(0,swingAngle*twistMultiplier,0);
+            rightHip.localEulerAngles=new Vector3(0,-swingAngle*twistMultiplier,0);
+            Vector3 offset = lockFoot.position-footPos;
+            body.position+=offset;
+            
+/*            float twistAmount = Mathf.Abs(swingAngVel*twistMultiplier*Time.deltaTime);
             if(firstStep)twistAmount*=0.5f;
             if(swingAngle<0)
             {
+                if(swingAngVel>0)twistAmount=0;
                 pivotBody(swingAngle*rockMultiplier,-twistAmount,leftFoot,rightFoot);
             }else
             {
+                if(swingAngVel<0)twistAmount=0;
                 pivotBody(swingAngle*rockMultiplier,twistAmount,rightFoot,leftFoot);            
-            }
+            }*/
         }
-        lastAngle=swingAngle;
+        prevAngle=swingAngle;
 	}    
     
     void pivotBody(float rockAngle,float twistAdd,Transform pivotFoot,Transform otherFoot)
