41cdd3eac2fcd1313d68f14b7a97989cd3a9e503
paultennent <paultennent@nottingham.ac.uk>
2017-02-21 11:03:44 +0000

    citizen stuff

---
+++ Assets/Scripts/Misc/CityBlockGen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityBlockGen : MonoBehaviour {
+
+	public GameObject[] buildings;
+
+	public float innerBlockCount = 5f;
+	private float buildingHeightScaleVariationMin = 0.2f;
+	private float buildingHeightScaleVariationMax = 10f;
+
+	// Use this for initialization
+	void Start () {
+		initBuild();
+	}
+	
+	private void initBuild(){
+		float rows = innerBlockCount;
+		while (rows > 0){
+			float xwidth = Random.Range(1,rows+1);
+			float cols = innerBlockCount;
+			while (cols > 0){
+				float zwidth = Random.Range(1,cols+1);
+				build(innerBlockCount-rows,innerBlockCount-cols,xwidth,zwidth);
+				cols -= zwidth;
+			}
+			rows -=xwidth;
+		}
+
+
+	}
+
+	private void build(float xpos,float zpos,float xwidth,float zwidth){
+		//get a random building
+		GameObject building = Instantiate(buildings[Random.Range(0,buildings.Length)]);
+		float height = 1f * Random.Range (buildingHeightScaleVariationMin, buildingHeightScaleVariationMax);
+		building.transform.parent = transform;
+		building.transform.localPosition = new Vector3(xpos, height/2f , zpos);
+		building.transform.localScale = new Vector3(xwidth,height,zwidth);
+
+	}
+}
---
+++ Assets/Scripts/Misc/CityGen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityGen : MonoBehaviour {
+
+	public int xBlocks = 1;
+	public int zBlocks = 1;
+
+	public float spacer = 1f;
+
+	public GameObject block;
+
+
+	// Use this for initialization
+	void Start () {
+
+		float blocksize = block.GetComponent<CityBlockGen> ().innerBlockCount;
+
+		for(int i=0;i<xBlocks;i++){
+			for (int j = 0; j < zBlocks; j++) {
+				float xpos = i * blocksize + i * spacer;
+				float zpos = j * blocksize + j * spacer;
+				GameObject newblock = Instantiate (block);
+				newblock.transform.position = new Vector3 (xpos, 0, zpos);
+				newblock.transform.parent = transform;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
