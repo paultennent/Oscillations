c7af428639b7aafc4125695b761ef53c3e30a5fe
Joe Marshall <joe.marshall@nottingham.ac.uk>
2017-10-13 11:43:56 +0100

    first version of orbit world

---
+++ Assets/Rides/OrbitAssets/OrbitCamMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamMover : AbstractGameEffects {
+
+    float ellipseW=20f;
+    float ellipseH=20f;
+    
+	// Use this for initialization
+	void Start () {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        base.Update();
+
+		if (!inSession) {
+			return;
+		}
+
+        if (!fadedIn)
+        {
+            FadeSphereScript.doFadeIn(5f, Color.black);
+            fadedIn = true;
+        }
+
+        if (!foundInitialPos)
+        {
+            foundInitialPos=true;
+            initialViewpointPos=viewpoint.transform.position;
+        }
+        
+        swingAngle;
+
+        
+        
+    }
+
+}
